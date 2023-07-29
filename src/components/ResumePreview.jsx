ca374440ab31c063db8787e1e6880b602f4b107a
Roy Joseph <royjoseph.mec@gmail.com>
2023-07-29 20:14:07 +0530

    Completed Top and Bottom section of The Preview.
    Now the preview shows the data entered in the personal data
    and contact section.(Other sections are yet to be made).

--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -1,19 +1,55 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone,faEnvelope,faGlobe,faMapLocationDot } from "@fortawesome/free-solid-svg-icons"
+
 function ResumePreview({data}) {
   return (
     <div className="ResumePreview">
         <div className="ResumeTop">
             <div className="Header">
                 <div className="HeadTag">
-                    <h1>Mighty Thor</h1>
+                    <h1>{data.info[0].text} {data.info[1].text}</h1>
                     <span>
-                        Front-End Developer
+                        {data.info[2].text}
                     </span>
                 </div>
                 <div className="imgContainer" style={data.fileSrc ? {backgroundColor:"none"} : {background:"black"}} >
                   <img src={data.fileSrc} alt={data.filename} />
                 </div>
             </div>
-            <p>From the moment I produced the very first "Hello World" in the console, I knew I was hooked into the world of software development. But software development is not "just a job" for me, it's offered an engaging challenge to continually learn and improve my skills in creating high-quality software.  It  became a full-fledged passion that only gets more exciting with time. Below you'll find projects I've completed that showcase my skills and general background in developing software.</p>
+            <p>{data.info[3].text}</p>
+        </div>
+        <div className="ResumeMiddle">
+            <h1>Experience</h1>
+            <div className="experienceContainer">
+                <div className="experience">
+                    <p>{data.experiences[0].position}</p>
+                </div>
+                <div className="experience">
+
+                </div>
+            </div>
+        </div>
+        <div className="BottomSection">
+            <div className="ColumnContainer">
+                <div className="sec">
+                    <FontAwesomeIcon icon={faMapLocationDot} size="xl" className="logos" />
+                    <p>{data.info[6].text}</p>
+                </div>
+                <div className="sec">
+                    <FontAwesomeIcon icon={faPhone} size="xl" className="logos" />
+                    <p>{data.info[7].text}</p>
+                </div>
+            </div>
+            <div className="ColumnContainer">
+                <div className="sec">
+                    <FontAwesomeIcon icon={faEnvelope} size="xl" className="logos" />
+                    <p>{data.info[8].text}</p>
+                </div>
+                <div className="sec">
+                    <FontAwesomeIcon icon={faGlobe} size="xl" className="logos" />
+                    <p>{data.info[9].text}</p>
+                 </div>
+            </div>
         </div>
     </div>
   )
